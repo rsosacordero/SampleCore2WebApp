886768c887aaf88aa7ddadbb215e929871d662c4
matthew.sherrill <matthew.sherrill@asynchrony.com>
2019-12-02 15:18:58 -0700

    Added ResourceNotFoundExc test - MS

--- EF-WWT.Tests.Unit/CQRS/SavePersonEmailCommandHandlerTests.cs
+++ EF-WWT.Tests.Unit/CQRS/SavePersonEmailCommandHandlerTests.cs
@@ -27,21 +27,9 @@ namespace EF_WWT.Tests.Unit.CQRS
         [Fact]
         public async Task Handler_PersonResourceDoesntExist_RNFExceptionThrown()
         {
-            var request = new SavePersonEmailCommand() { PersonIdentifier = Guid.NewGuid(), EmailAddress = "123@123.com" };
-            //try
-            //{
-            //    Action act = () =>  await _handler.TestHandle(request, new System.Threading.CancellationToken());
-            //}
-            //catch(Exception exc)
-            //{
-            //    exc.Should().BeOfType<ResourceNotFoundException>();
-            //}
-
-            //Action act = async () =>  await _handler.TestHandle(request, new System.Threading.CancellationToken());
-            //act.Should().ThrowExactly<ResourceNotFoundException>();
-
-
-
+            var request = new SavePersonEmailCommand() { PersonIdentifier = Guid.NewGuid(), EmailAddress = "123@123.com" };           
+            Func<Task> func = async () => await _handler.TestHandle(request, new System.Threading.CancellationToken());
+            func.Should().ThrowExactly<ResourceNotFoundException>();
         }
 
 
